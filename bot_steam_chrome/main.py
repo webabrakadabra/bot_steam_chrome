cf18281e737d61170da43891c70bcaef0c0a8f73
grey <hireshnik@gmail.com>
2022-01-13 16:27:24 +0200

    1

--- bot_steam_chrome/main.py
+++ bot_steam_chrome/main.py
@@ -1,5 +1,10 @@
-from selenium import webdriver
-from config import *
+#from selenium import webdriver
+from text import *
 
-browser = webdriver.Chrome(path_chrome_browser_driver)
-browser.get(start_site_url)
+#browser = webdriver.Chrome(path_chrome_browser_driver)
+#browser.get(start_site_url)
+import sys
+
+
+print(sys.path)
+print(var)
