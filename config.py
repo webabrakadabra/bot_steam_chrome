cc250c31cd691757a25ed7d4555de71177ecd1e6
grey <hireshnik@gmail.com>
2022-01-17 08:57:08 +0200

    x

---
+++ config.py
@@ -0,0 +1,2 @@
+path_chrome_browser_driver = "chromedriver_linux64_96.0.4664.45"
+start_site_url = "https://steamcommunity.com/"
--- config/config.py
+++
@@ -1,1 +0,0 @@
-path_chrome_browser_driver = "../lib/chromedriver_linux64_96.0.4664.45"
