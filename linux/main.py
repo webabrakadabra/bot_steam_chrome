17838642afbce4b86289c773df4f19eab5e4c6c9
grey <hireshnik@gmail.com>
2022-02-17 15:39:28 +0200

    linux

--- linux/main.py
+++ linux/main.py
@@ -4,8 +4,10 @@ from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
 
 driver = webdriver.Chrome()
-wait = WebDriverWait(driver, 10)
+#wait = WebDriverWait(driver, 10)
 driver.maximize_window()
 #print(dir(webdriver))
 driver.get("https://store.steampowered.com/")
-#wait.until(EC.element_to_be_clickable((By.ID, "steamAccountName"))).send_keys('username')
+#wait.until(EC.element_to_be_clickable((By.XPATH, '//*[@id="global_action_menu"]/a'))).click()
+#//*[@id="global_action_menu"]/a
+login = driver.find_elements(By.ID, 'language_dropdown').click()
