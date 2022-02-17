7db59c93b6aac14da8540cee4ceeaedc21eae766
webabrakadabra <hireshnik@gmail.com>
2022-02-17 20:08:39 +0200

    kkk

--- linux/main.py
+++ linux/main.py
@@ -2,12 +2,13 @@ from selenium import webdriver
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
 
 driver = webdriver.Chrome()
-#wait = WebDriverWait(driver, 10)
+wait = WebDriverWait(driver, 10)
 driver.maximize_window()
 #print(dir(webdriver))
-driver.get("https://store.steampowered.com/")
-#wait.until(EC.element_to_be_clickable((By.XPATH, '//*[@id="global_action_menu"]/a'))).click()
+driver.get("http://store.steampowered.com")
+wait.until(EC.element_to_be_clickable((By.XPATH, '//*[@id="global_action_menu"]/a'))).send_keys(Keys.ENTER)
 #//*[@id="global_action_menu"]/a
-login = driver.find_elements(By.ID, 'language_dropdown')
+# driver.find_element(By.XPATH, '//*[@id="global_action_menu"]/a').send_keys(Keys.ENTER)
